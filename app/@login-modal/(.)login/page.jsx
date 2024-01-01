76cfc427a59b178258bd1c556980413c7ad392d9
cobbygraves <rmeocnis@hotmail.com>
2024-01-01 12:22:20 +0000

    removed login page

--- app/@login-modal/(.)login/page.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const InterceptedLogin = () => {
-  return <div>Intercepted Login Form</div>
-}
-
-export default InterceptedLogin
--- app/@login-modal/default.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const DefaultLogin = () => {
-  return <div>DefaultLogin Page</div>
-}
-
-export default DefaultLogin
